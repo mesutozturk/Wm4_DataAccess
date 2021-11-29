98451bc363971521e1327e04180d4fd0628aeac8
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2021-11-29 14:47:31 +0300

    Add project files.

---
+++ North_DbFirst/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace North_DbFirst
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvNorth = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNorth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvNorth
+            // 
+            this.dgvNorth.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNorth.Location = new System.Drawing.Point(12, 12);
+            this.dgvNorth.Name = "dgvNorth";
+            this.dgvNorth.RowHeadersWidth = 51;
+            this.dgvNorth.RowTemplate.Height = 29;
+            this.dgvNorth.Size = new System.Drawing.Size(776, 426);
+            this.dgvNorth.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvNorth);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNorth)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvNorth;
+    }
+}
+
---
+++ North_DbFirst/Form1.cs
@@ -0,0 +1,52 @@
+﻿using North_DbFirst.Models;
+using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using North_DbFirst.ViewModels;
+
+namespace North_DbFirst
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private NorthwindContext _dContext = new NorthwindContext();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var query1 = _dContext.Categories.Select(x => new CategoryViewModel()
+            {
+                CategoryName = x.CategoryName,
+                Description = x.Description,
+                Picture = x.Picture,
+                ProductCount = x.Products.Count
+            }).ToList();
+            dgvNorth.DataSource = query1;
+
+            var query2 = from cat in _dContext.Categories
+                         join prod in _dContext.Products on cat.CategoryId equals prod.CategoryId
+                         //where prod.UnitPrice > 20
+                         select new
+                         {
+                             cat.CategoryName,
+                             prod.ProductName,
+                             prod.UnitPrice
+                         };
+            dgvNorth.DataSource = query2
+                .OrderBy(x => x.CategoryName)
+                .ThenByDescending(x => x.UnitPrice)
+                .ToList();
+
+            var query3 = _dContext.Products.Select(x => new
+            {
+                x.Category.CategoryName,
+                x.ProductName,
+                x.UnitPrice
+            }).OrderBy(x => x.CategoryName).ThenByDescending(x => x.UnitPrice).ToList();
+            dgvNorth.DataSource = query3;
+        }
+    }
+}
