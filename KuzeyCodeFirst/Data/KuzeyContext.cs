8523f732f1a4a6b5b0c0af3992432f96da0ff8c8
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2021-12-06 10:02:33 +0300

    codefirst 1

---
+++ KuzeyCodeFirst/Data/KuzeyContext.cs
@@ -0,0 +1,17 @@
+﻿using KuzeyCodeFirst.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KuzeyCodeFirst.Data
+{
+    public class KuzeyContext : DbContext
+    {
+        public KuzeyContext()
+            :base()
+        {
+            
+        }
+
+        public DbSet<Kategori> Kategoriler { get; set; }
+        public DbSet<Urun> Urunler { get; set; }
+    }
+}
---
+++ KuzeyCodeFirst/Models/Kategori.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KuzeyCodeFirst.Models
+{
+    [Table("Kategoriler")]
+    public class Kategori
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Ad alanı boş geçilemez")]
+        [StringLength(30, ErrorMessage = "Ad alanı en fazla 30 karakter olur")]
+        public string Ad { get; set; }
+        [StringLength(250)]
+        public string Aciklama { get; set; }
+
+        public ICollection<Urun> Urunler { get; set; } = new HashSet<Urun>();
+    }
+}
