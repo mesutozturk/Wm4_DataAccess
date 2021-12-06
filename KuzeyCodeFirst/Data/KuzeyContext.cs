d8b8a2af4ab0c925eeaf4e73cd521d22518b6682
Mesut ÖZTÜRK <mesutoztrk@msn.com>
2021-12-06 11:12:55 +0300

    codefirst 2

--- KuzeyCodeFirst/Data/KuzeyContext.cs
+++ KuzeyCodeFirst/Data/KuzeyContext.cs
@@ -6,12 +6,27 @@ namespace KuzeyCodeFirst.Data
     public class KuzeyContext : DbContext
     {
         public KuzeyContext()
-            :base()
+            : base()
         {
-            
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Kuzey4Db;Integrated Security=True;");
+            }
         }
 
         public DbSet<Kategori> Kategoriler { get; set; }
         public DbSet<Urun> Urunler { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Urun>()
+                .Property(x => x.Fiyat)
+                .HasPrecision(10, 2);
+        }
     }
 }
